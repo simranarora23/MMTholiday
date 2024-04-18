517fcdbcf909530fd70f4859b30da5465232e780
simran2310 <simran.arora@capgemini.com>
2024-04-18 14:30:50 +0530

    HolidayPackage

---
+++ MMT_HolidayPackage/src/test/java/MMT/MMT_HolidayPackage/AppTest.java
@@ -0,0 +1,20 @@
+package MMT.MMT_HolidayPackage;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
